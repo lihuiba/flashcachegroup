f38f6cdacb3a95a962f8104482bc28c8fbb3ece5
Ziyang Li <lzynudt@gmail.com>
2013-06-13 09:14:39 -0400

    show more details about a flashcache group

--- fcg-show.py
+++ fcg-show.py
@@ -19,7 +19,23 @@ def parse_args(cmdline):
 def show_group(groupName):
     cmd = 'dmsetup table %s' % groupName
     groupTable = FcgUtils.os_execue(cmd)
-    print groupTable
+    hardDisks = []
+    cachedHardDisks = []
+    for groupLine in groupTable.split('\n'):
+        groupLine = groupLine.split()
+        if len(groupLine) == 5:
+            startGroupSec, offsetGroup, typeGroup, oriGroupDev, oriGroupStartSec = groupLine
+            startGroupSec, offsetGroup, oriGroupStartSec = map(int, [startGroupSec, offsetGroup, oriGroupStartSec])
+            oriGroupDev = FcgUtils.get_devname_from_major_minor(oriGroupDev)
+            hardDisks.append(oriGroupDev)
+            cachedHardDisk = '/dev/mapper/cache_%s' % oriGroupDev.split('/')[-1:][0]
+            cachedHardDisks.append(cachedHardDisk)
+    print 'Flashcache group name:', groupName
+    print 'Consists of hard disks:'
+    print '\t', '\t'.join(hardDisks)
+    print 'Serves following cached disks:'
+    print '\t', '\t'.join(cachedHardDisks)
+    
 
 if __name__ == '__main__':
     groupName = parse_args(sys.argv[1:])
