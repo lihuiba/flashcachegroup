5c3cb52eadb682f8e5b95bb0f6eed2610aa4e668
Ziyang Li <lzynudt@gmail.com>
2013-09-06 09:56:11 +0800

    add setup.py, fix a small bug

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name=NAME,
     version="0.2.3",
     description=DESCRIPTION,
-    long_description=open("README.md").read()
+    long_description=open("README.md").read(),
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license="GPL",
