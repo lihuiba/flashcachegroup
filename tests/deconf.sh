f8c534ef1923ea8a883a10c4fd918920ff4f54b1
Ziyang Li <lzynudt@gmail.com>
2014-02-21 12:38:00 +0800

    update test

--- tests/deconf.sh
+++ tests/deconf.sh
@@ -1,7 +1,6 @@
 dmsetup remove cached_loop0
 dmsetup remove cached_loop1
 dmsetup remove cached_loop2
-dmsetup remove free_tfcg
 dmsetup remove cache_tfcg
 dmsetup remove ssd_tfcg
 dmsetup remove tfcg
