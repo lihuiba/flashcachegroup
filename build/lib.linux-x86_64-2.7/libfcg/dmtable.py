491711268caddd9a2717a081a8b183bb94f47974
Ziyang Li <lzynudt@gmail.com>
2014-03-18 16:37:30 +0800

    version 0.5.0

---
+++ build/lib.linux-x86_64-2.7/libfcg/dmtable.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import os
+from libfcg import utils
+
+def get_error_table(sectors):
+	table = '0 %d error' % sectors
+	return table
+
+def linear_map_table(disks):
+	table = ''
+	startSector = 0
+	for disk in disks:
+		sector = utils.get_dev_sector_count(disk)
+		table +=  '%d %d linear %s 0\n' % (startSector, sector, disk)
+		startSector += sector
+	return table
+
+def get_disks_in_linear_table(table):
+	disks = []
+	table_list = table.split('\n')
+	for table_line_str in table_list:
+		line = table_line_str.split()
+		if len(line) == 5:
+			disk = line[3]
+			try:
+				major, minor = [int(x) for x in disk.split(':')]
+				disk = utils.get_devname_from_major_minor(disk)
+			except Exception, e:
+				pass
+			disks.append(disk)
+	return disks
+
+def insert_disk_to_linear_table(disk, table):
+	new_table = ''
+	sector = utils.get_dev_sector_count(disk)
+	lines = table.strip().split('\n')
+	for i in range(len(lines)):
+		line_str = lines[i]
+		line = line_str.strip().split()
+		start, offset = map(int, line[0:2])
+		map_type = line[2]
+		if map_type == 'error' and offset >= sector:
+			new_disk_line = '%d %d linear %s 0\n' % (start, sector, disk)	
+			new_table += new_disk_line
+			if offset > sector:
+				new_err_line = '%d %d error\n' %(start+sector, offset-sector)
+				new_table += new_err_line
+		else:
+			new_table += line_str
+			new_table += '\n'
+	return new_table
+				
---
+++ build/lib.linux-x86_64-2.7/libfcg/utils.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import os
+import tempfile
+
+from libfcg.common import processutils as putils
+
+def execute(cmd, *args, **kwargs):
+	(out, ret) = putils.execute(cmd, *args, **kwargs)
+	out = out.strip()
+	return out
+
+def get_dev_sector_count(dev):
+	if not os.path.exists(dev):
+		raise Exception('Device %s does NOT exist...' % dev)
+	devSector = execute('blockdev', '--getsz', dev)
+	if type(devSector) != int:
+		try:
+			devSector = int(devSector)
+		except:
+			return 0
+	if devSector <= 0:
+		raise Exception('Device %s is EMPTY...' % dev)
+	return devSector
+
+def get_devname_from_major_minor(major_minor):
+	return '/dev/' + os.readlink('/dev/block/%s' % major_minor)[3:]
+
+def write2tempfile(content):
+	temp = tempfile.NamedTemporaryFile(delete=False)
+	temp.write(content)
+	temp.close()
+	return temp.name
+
+def bytes_str2bytes_count(bytes):
+	#take M as default
+	bytes_num = 0
+	if bytes.endswith('P') or bytes.endswith('p'):
+		bytes_num = int(bytes[:-1])*1024*1024*1024*1024*1024
+	elif bytes.endswith('T') or bytes.endswith('t'):
+		bytes_num = int(bytes[:-1])*1024*1024*1024*1024
+	elif bytes.endswith('G') or bytes.endswith('g'):
+		bytes_num = int(bytes[:-1])*1024*1024*1024
+	elif bytes.endswith('M') or bytes.endswith('m'):
+		bytes_num = int(bytes[:-1])*1024*1024
+	elif bytes.endswith('K') or bytes.endswith('k'):
+		bytes_num = int(bytes[:-1])*1024
+	else:
+		bytes_num = int(bytes)*1024*1024
+	return bytes_num
+
+def bytes2sectors(bytes):
+	bytes = bytes_str2bytes_count(bytes)
+	sectors = bytes/512
+	return sectors
+
+def sector_offset2block_offset(startSector, offset, blkSize):
+	blkSize = bytes_str2bytes_count(blkSize)
+	startBlk = startSector * 512 / blkSize
+	offsetBlk = offset * 512 / blkSize
+	return startBlk, offsetBlk
+
+def sectors2MB(sectors):
+	return str(sectors*512/1024/1024) + 'M'
+
+if __name__ == '__main__':
+	print get_dev_sector_count('/dev/sda5')
