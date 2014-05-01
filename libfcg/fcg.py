6a05fab59ba24eca0b2e6b77d2ddfe919a0ec0c9
Ziyang Li <lzynudt@gmail.com>
2014-05-02 00:00:22 +0800

    add root helper

--- libfcg/fcg.py
+++ libfcg/fcg.py
@@ -5,15 +5,15 @@ import os
 from pydm.dmsetup import Dmsetup
 from pydm.dmtable import LinearTable
 from pydm.disk import Disk
-from libfcg.common import executor
-from libfcg.common import processutils as putils
 from libfcg import utils
 from libfcg.flashcache import Flashcache
 
+
 class FCG():
 
-    def __init__(self, group_name):
+    def __init__(self, group_name, root_helper=''):
         self.group_name = group_name
+        self.root_helper = root_helper
 
     def _ssd_name(self):
         return 'ssd_' + self.group_name
@@ -31,29 +31,28 @@ class FCG():
     def create_group(self, ssds, block_size, pattern):
         #TODO: roll back if failed
         group_name = self.group_name
-        dm = Dmsetup()
         hdd_size = '1P'
         hdd_sectors = utils.bytes2sectors(hdd_size)
-        hdd = Disk.from_error(hdd_sectors)
-        hdd_group = LinearTable.from_disks(group_name, [hdd])
+        hdd = Disk.from_error(hdd_sectors, root_helper=self.root_helper)
+        hdd_group = LinearTable.from_disks(group_name, [hdd], root_helper=self.root_helper)
 
         ssd_name = self._ssd_name()
-        ssd_group = LinearTable.from_disks(ssd_name, ssds)
+        ssd_group = LinearTable.from_disks(ssd_name, ssds,  root_helper=self.root_helper)
 
-        fc = Flashcache()
+        fc = Flashcache(root_helper=self.root_helper)
         cache_name = self._cache_name()
-        cache_Dev = fc.create(cache_name, ssd_group.path, hdd_group.path, block_size, pattern)
-        return cache_Dev
+        cache_dev = fc.create(cache_name, ssd_group.path, hdd_group.path, block_size, pattern)
+        return cache_dev
 
     def add_disk(self, disk_path):
         if os.path.islink(disk_path):
             disk_path = os.path.realpath(disk_path)
-        disk = Disk.from_path(disk_path)
-        hdd_group = LinearTable(self.group_name)
+        disk = Disk.from_path(disk_path, root_helper=self.root_helper)
+        hdd_group = LinearTable(self.group_name, root_helper=self.root_helper)
         assert hdd_group.existed, "Group %s dose NOT exist..." % self.group_name
 
         hdd_group.insert_disk(disk)
-        dm = Dmsetup() 
+        dm = Dmsetup(root_helper=self.root_helper)
         cache_dev = dm.mapdev_prefix + self._cache_name()
         cached_table = '0 %d linear %s %d' % (disk.size, cache_dev, disk.start)
         cached_disk_name = self._cached_disk_name(disk.dev)
@@ -64,17 +63,17 @@ class FCG():
     def rm_disk(self, disk_path):
         if os.path.islink(disk_path):
             disk_path = os.path.realpath(disk_path)
-        hdd_group = LinearTable(self.group_name)
+        hdd_group = LinearTable(self.group_name, root_helper=self.root_helper)
         assert hdd_group.existed, "Group %s dose NOT exist..." % self.group_name
         disk = hdd_group.find_disk(disk_path)
 
-        dm = Dmsetup()
+        dm = Dmsetup(root_helper=self.root_helper)
         cached_name = self._cached_disk_name(disk.dev)
 
         cache_name = self._cache_name()
         cache_dev = dm.mapdev_prefix + cache_name
         cache_table = dm.get_table(cache_name)
-        fc = Flashcache()
+        fc = Flashcache(root_helper=self.root_helper)
         block_size = fc.get_block_size(cache_table)
         start_blk, offset_blk = utils.sector_offset2block_offset(disk.start, disk.size, block_size)
         fc.invalid(cache_dev, start_blk, offset_blk)
@@ -83,16 +82,16 @@ class FCG():
         hdd_group.remove_disk(disk)
 
     def delete_group(self):
-        hdd_group = LinearTable(self.group_name)
+        hdd_group = LinearTable(self.group_name, root_helper=self.root_helper)
         assert hdd_group.existed, "Group %s dose NOT exist..." % self.group_name
-        dm = Dmsetup()
+        dm = Dmsetup(root_helper=self.root_helper)
         #TODO: check for wether busy
         for disk in hdd_group.disks:
             if disk.mapper != 'error':
                 cached_name = self._cached_disk_name(disk.dev)
                 dm.remove_table(cached_name)
 
-        fc = Flashcache()
+        fc = Flashcache(root_helper=self.root_helper)
         cache_name = self._cache_name()
         cache_table = dm.get_table(cache_name)
         ssd_dev = fc.get_ssd_dev(cache_table)
--- libfcg/flashcache.py
+++ libfcg/flashcache.py
@@ -4,15 +4,15 @@ from libfcg.common import executor
 from libfcg.common import processutils as putils
 from libfcg import utils
 
-
 cachedev_prefix = '/dev/mapper/'
 
+
 class Flashcache(executor.Executor):
-    def __init__(self, execute=putils.execute):
-        super(Flashcache, self).__init__(root_helper='', execute=execute)
+    def __init__(self, root_helper='', execute=putils.execute):
+        super(Flashcache, self).__init__(root_helper=root_helper, execute=execute)
 
     def _run(self, cmd, *args, **kwargs):
-        self._execute(cmd, *args, run_as_root=True, **kwargs)
+        self._execute(cmd, *args, run_as_root=True, root_helper=self._root_helper, **kwargs)
 
     def create(self, cache_name, ssd_dev, group_dev, block_size, pattern):
         try:
@@ -21,7 +21,6 @@ class Flashcache(executor.Executor):
             pass
 
         cache_size = utils.sectors2MB(utils.get_dev_sector_count(ssd_dev))
-        #print ' '.join(['flashcache_create', '-p', pattern, '-b', block_size, '-s', cache_size, cache_name, ssd_dev, group_dev])
         self._run('flashcache_create', '-p', pattern, '-b', block_size, '-s', cache_size, cache_name, ssd_dev, group_dev)
         return cachedev_prefix + cache_name
         
