53b60a7a00c84fe9b58f2c38aeba770c5aef0472
anzigly <anzigly@gmail.com>
2014-08-12 20:16:52 +0800

    roll back if failed

--- libfcg/fcg.py
+++ libfcg/fcg.py
@@ -9,13 +9,17 @@ from pydm.dmlinear import DmLinearTable
 
 from libfcg import utils
 from libfcg.flashcache import Flashcache
+from libfcg.common.chain import Chain
 
 
 class FCG():
-
     def __init__(self, group_name, root_helper=''):
         self.group_name = group_name
         self.root_helper = root_helper
+        self.hdd_group = None
+        self.ssd_group = None
+        self.flashcache = None
+        self.cache_dev = ''
 
     @property
     def _ssd_name(self):
@@ -57,30 +61,47 @@ class FCG():
         assert hdd_group.existed, "Group %s dose NOT exist..." % self.group_name
         return self._exists(hdd_group, disk)
 
-    def create_group(self, ssds, block_size, pattern):
-        #TODO: roll back if failed
+    def _create_hdd_group(self):
         group_name = self.group_name
         hdd_size = '1P'
         hdd_sectors = utils.bytes2sectors(hdd_size)
         hdd = Disk.from_error(hdd_sectors, root_helper=self.root_helper)
-        hdd_group = DmLinearTable.from_disks(group_name, [hdd], root_helper=self.root_helper)
+        self.hdd_group = DmLinearTable.from_disks(group_name, [hdd], root_helper=self.root_helper)
 
+    def _create_ssd_group(self, ssds):
         ssd_name = self._ssd_name
-        ssd_group = DmLinearTable.from_disks(ssd_name, ssds,  root_helper=self.root_helper)
+        self.ssd_group = DmLinearTable.from_disks(ssd_name, ssds, root_helper=self.root_helper)
 
-        fc = Flashcache(root_helper=self.root_helper)
+    def _create_cache_group(self, block_size, pattern):
+        self.flashcache = Flashcache(root_helper=self.root_helper)
         cache_name = self._cache_name
-        cache_dev = fc.create(cache_name, ssd_group.path, hdd_group.path, block_size, pattern)
-        return cache_dev
+        self.cache_dev = self.flashcache.create(cache_name, self.ssd_group.path, self.hdd_group.path, block_size, pattern)
+
+    def _destroy_cache(self):
+        dm = Dmsetup(root_helper=self.root_helper)
+        cache_name = self._cache_name
+        cache_table = dm.get_table(cache_name)
+        ssd_dev = self.flashcache.get_ssd_dev(cache_table)
+        dm.remove_table(cache_name)
+        self.flashcache.destroy(ssd_dev)
+
+    def create_group(self, ssds, block_size, pattern):
+        create_chain = Chain()
+        create_chain.add_step(lambda: self._create_hdd_group(), lambda: self.hdd_group.remove_table())
+        create_chain.add_step(lambda: self._create_ssd_group(ssds), lambda: self.ssd_group.remove_table())
+        create_chain.add_step(lambda: self._create_cache_group(block_size, pattern), lambda: self._destroy_cache())
+        create_chain.do()
+        return self.cache_dev
 
     def add_disk(self, disk_path):
         if os.path.islink(disk_path):
             disk_path = os.path.realpath(disk_path)
         disk = Disk.from_path(disk_path, root_helper=self.root_helper)
-        hdd_group = DmLinearTable(self.group_name, root_helper=self.root_helper)
-        assert hdd_group.existed, "Group %s dose NOT exist..." % self.group_name
-        if not hdd_group.exists(disk):
-            hdd_group.append_disk(disk)
+        if not self.hdd_group:
+            self.hdd_group = DmLinearTable(self.group_name, root_helper=self.root_helper)
+        assert self.hdd_group.existed, "Group %s dose NOT exist..." % self.group_name
+        if not self.hdd_group.exists(disk):
+            self.hdd_group.append_disk(disk)
         dm = Dmsetup(root_helper=self.root_helper)
         cached_disk_name = self._cached_disk_name(disk.dev)
         if not dm.is_exist(cached_disk_name):
@@ -92,30 +113,27 @@ class FCG():
     def rm_disk(self, disk_path):
         if os.path.islink(disk_path):
             disk_path = os.path.realpath(disk_path)
-        hdd_group = DmLinearTable(self.group_name, root_helper=self.root_helper)
-        assert hdd_group.existed, "Group %s dose NOT exist..." % self.group_name
-        disk = hdd_group.find_disk(disk_path)
+        if not self.hdd_group:
+            self.hdd_group = DmLinearTable(self.group_name, root_helper=self.root_helper)
+        assert self.hdd_group.existed, "Group %s dose NOT exist..." % self.group_name
+        disk = self.hdd_group.find_disk(disk_path)
         dm = Dmsetup(root_helper=self.root_helper)
         cached_name = self._cached_disk_name(disk.dev)
         dm.remove_table(cached_name)
-        hdd_group.remove_disk(disk)
+        self.hdd_group.remove_disk(disk)
 
     def delete_group(self):
-        hdd_group = DmLinearTable(self.group_name, root_helper=self.root_helper)
-        assert hdd_group.existed, "Group %s dose NOT exist..." % self.group_name
+        if not self.hdd_group:
+            self.hdd_group = DmLinearTable(self.group_name, root_helper=self.root_helper)
+        assert self.hdd_group.existed, "Group %s dose NOT exist..." % self.group_name
         dm = Dmsetup(root_helper=self.root_helper)
-        #TODO: check for wether busy
-        for disk in hdd_group.disks:
+        # TODO: check for wether busy
+        for disk in self.hdd_group.disks:
             if disk.mapper != 'error':
                 cached_name = self._cached_disk_name(disk.dev)
                 dm.remove_table(cached_name)
 
-        fc = Flashcache(root_helper=self.root_helper)
-        cache_name = self._cache_name
-        cache_table = dm.get_table(cache_name)
-        ssd_dev = fc.get_ssd_dev(cache_table)
-        dm.remove_table(cache_name)
-        fc.destroy(ssd_dev)
+        self._destroy_cache()
 
         time.sleep(0.1)
 
